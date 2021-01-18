95bb97038a30c4edc8e0663745889beea909c458
freshjy <freshjeongyeop@gmail.com>
2021-01-18 10:04:58 +0900

    mysite05 #1,2,3

--- mysite05/src/main/java/com/markany/mysite/initializer/MySiteWebApplicationInitializer.java
+++ mysite05/src/main/java/com/markany/mysite/initializer/MySiteWebApplicationInitializer.java
@@ -9,40 +9,42 @@ import org.springframework.web.servlet.FrameworkServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import com.markany.mysite.config.AppConfig;
+import com.markany.mysite.config.WebConfig;
 
 public class MySiteWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
 		// Root Application Context Config
-		return new Class<?>[] {AppConfig.class };
+		return new Class<?>[] { AppConfig.class };
 	}
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
 		// Web Application Context Config
-		return new Class<?>[] {AppConfig.class };
+		return new Class<?>[] { WebConfig.class };
 	}
 
 	@Override
 	protected String[] getServletMappings() {
-		// DipatcherServlet Mapping
+		// DispatcherServlet Mapping
 		return new String[] { "/" };
 	}
 
 	@Override
 	protected FrameworkServlet createDispatcherServlet(WebApplicationContext servletAppContext) {
 		// create DispatcherServlet
-		DispatcherServlet dispatcherServlet = (DispatcherServlet)super.createDispatcherServlet(servletAppContext);
+		DispatcherServlet diapatcherServlet = (DispatcherServlet)super.createDispatcherServlet(servletAppContext);
+	
+		// Exception Handler가 없으면 Exception을 던지게끔 설정
+		diapatcherServlet.setThrowExceptionIfNoHandlerFound(true);
 		
-		// Exception Handler가 없으면
-		dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
-		return dispatcherServlet;
+		return diapatcherServlet;
 	}
 
 	@Override
 	protected Filter[] getServletFilters() {
-		return new Filter[] { new CharacterEncodingFilter("UTF-8", true)};
+		return new Filter[] { new CharacterEncodingFilter("UTF-8", true) };
 	}
 	
 }
